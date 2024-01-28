e1d363967b5cb22bd2c99b66cd126e47548458f5
windkindchen <may-britt.thiessen@gmx.de>
2024-01-28 17:13:30 +0100

    Update fcverw.php

--- inc/plugins/fcverw.php
+++ inc/plugins/fcverw.php
@@ -43,6 +43,7 @@
        `lbesp` int(1) NOT NULL DEFAULT 0,
        `lstat` int(1) NOT NULL DEFAULT 0,
        `luebergeordnet` int(15) NOT NULL,
+			 `lverantwortl` int(15) NOT NULL,
        PRIMARY KEY (`landid`)
       )
       ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=1
@@ -54,7 +55,7 @@
         `diplandid` int(15) NOT NULL,
         `dippartid` int(15) NOT NULL,
         `dipstatus` int(2) NOT NULL,
-        `dipcom` text NOT NULL,
+        `dipbeschr` text NOT NULL,
         `diptime` timestamp NOT NULL DEFAULT current_timestamp(),
         PRIMARY KEY (`dipid`)
       )
@@ -64,7 +65,7 @@
     $db->write_query("
       CREATE TABLE ".TABLE_PREFIX."laender_info (
         `linfoid` int(15) UNSIGNED NOT NULL AUTO_INCREMENT,
-        `lid` int(15) NOT NULL,
+        `landid` int(15) NOT NULL,
         `lidatum` timestamp NOT NULL DEFAULT current_timestamp(),
         `lifreigabe` int(1) NOT NULL DEFAULT 0,
         `sprache` longtext NOT NULL,
@@ -90,7 +91,7 @@
       CREATE TABLE ".TABLE_PREFIX."laender_kontinente (
         `kid` int(15) UNSIGNED NOT NULL AUTO_INCREMENT,
         `kname` varchar(255) NOT NULL,
-        `kbeschr` text,
+        `kbeschr` text NOT NULL,
         PRIMARY KEY (`kid`)
       )
       ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=1
@@ -101,7 +102,7 @@
         `rid` int(15) UNSIGNED NOT NULL AUTO_INCREMENT,
         `rkid` int(15) NOT NULL,
         `rname` varchar(255) NOT NULL,
-        `kbeschr` text,
+        `rbeschr` text NOT NULL,
         PRIMARY KEY (`rid`)
       )
       ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=1
@@ -112,8 +113,22 @@
         `lvid` int(15) UNSIGNED NOT NULL AUTO_INCREMENT,
         `landid` int(15) NOT NULL,
         `verwid` int(15) NOT NULL,
-        `lvbeschr` text,
+        `lvbeschr` text NOT NULL,
         PRIMARY KEY (`lvid`)
+      )
+      ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=1
+    ");
+
+		$db->write_query("
+      CREATE TABLE ".TABLE_PREFIX."laender_personen (
+        `lpid` int(15) UNSIGNED NOT NULL AUTO_INCREMENT,
+        `landid` int(15) NOT NULL,
+        `uid` int(15) NOT NULL,
+        `name` varchar(255) NOT NULL,
+				`titel` varchar(255) NOT NULL,
+				`rang` int(15) NOT NULL,
+				`lpfreigabe` int(1) NULL DEFAULT 0,
+        PRIMARY KEY (`lpid`)
       )
       ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=1
     ");
@@ -164,6 +179,10 @@
     {
       $db->drop_table("laender_verwandt");
     }
+		if ($db->table_exists("laender_personen"))
+    {
+      $db->drop_table("laender_personen");
+    }
 
 
     // Löschen der Einstellungen
@@ -191,6 +210,363 @@
   }
 
 
-/* ********************************************
-            Let the magic begin
-******************************************** */
+/* **********************************************************************************************
+   **********************************************************************************************
+                                          Let the magic begin
+   **********************************************************************************************
+   ********************************************************************************************** */
+
+
+/* ***********************************************
+          Part 01: ADMIN-CP
+   *********************************************** */
+
+   #######################################
+   ### // creates link in acp -> users ###
+   #######################################
+
+   	$plugins->add_hook('admin_config_menu', 'fcverw_admin_config_menu');
+   	function fcverw_admin_config_menu(&$sub_menu)
+   	{
+
+   	    $sub_menu[] = array(
+   	        'id' => 'fcverw',
+   	        'title' => 'L&auml;nderverwaltung',
+   	        'link' => 'index.php?module=config-fcverw'
+   	    );
+   	}
+
+
+
+    #################################################
+    ### // hook actions for fcverw management ###
+    #################################################
+
+    $plugins->add_hook('admin_config_action_handler', 'fcverw_admin_config_action_handler');
+    function fcverw_admin_config_action_handler(&$actions)
+    {
+        $actions['fcverw'] = array('active' => 'fcverw', 'file' => 'fcverw');
+    }
+
+
+
+    $plugins->add_hook('admin_load', 'fcverw_admin');
+  	function fcverw_admin()
+  	{
+  		global $mybb, $db, $lang, $page, $action_file, $run_module, $form, $land, $i;
+
+
+  		if ($page->active_action != 'fcverw')
+  		{
+          	return false;
+      }
+
+
+
+      if ($run_module == 'config' && $action_file == 'fcverw')
+  		{
+      		$page->add_breadcrumb_item('L&auml;nderverwaltung', 'index.php?module=config-fcverw');
+
+
+  // now let us create the tabs (tab, tab, tib)
+          $sub_tabs['laender'] = array(
+            'title'	=> 'Alle L&auml;nder',
+            'link'	=> 'index.php?module=config-fcverw',
+            'description'   => '&Uuml;bersicht aller L&auml;nder<br />
+  					                   <img src="fcverw/frei.png"> Land frei; <img src="fcverw/vergeben.png"> Land vergeben; <img src="fcverw/error.png"> Land vergeben ohne Verantwortlichen'
+          );
+          $sub_tabs['add_land'] = array(
+            'title' => 'Land anlegen',
+  				  'link' => 'index.php?module=config-fcverw&amp;action=add_land',
+  					'description' => 'Anlegen eines neuen Landes'
+  				);
+          $sub_tabs['regionen'] = array(
+            'title' => 'Regionen',
+  				  'link' => 'index.php?module=config-fcverw&amp;action=regionen',
+  					'description' => 'Übersicht der Regionen'
+  				);
+          $sub_tabs['add_region'] = array(
+            'title' => 'Region anlegen',
+  				  'link' => 'index.php?module=config-fcverw&amp;action=add_region',
+  					'description' => 'Anlegen eines neuen Landes'
+  				);
+          $sub_tabs['kontinente'] = array(
+            'title' => 'Kontinente',
+            'link' => 'index.php?module=config-fcverw&amp;action=kontinente',
+            'description' => 'Übersicht der Kontinente'
+          );
+          $sub_tabs['add_kontinent'] = array(
+            'title' => 'Kontinent anlegen',
+  				  'link' => 'index.php?module=config-fcverw&amp;action=add_kontinent',
+  					'description' => 'Anlegen eines neuen Kontinents'
+  				);
+
+
+
+
+  // start the fire with showing ALL countries, while there is neither set action
+  // nor it is show_all. Yay!
+  			if ($mybb->input['action'] == "" || !$mybb->input['action'] || $mybb->input['action'] == 'laender')
+  			{
+          $page->add_breadcrumb_item('&Uuml;bersicht aller L&auml;nder');
+
+          $page->output_header('L&auml;nderverwaltung - &Uuml;bersicht aller L&auml;nder');
+
+          // which tab is selected?
+          $page->output_nav_tabs($sub_tabs, 'laender');
+
+          // now start the form
+          $form = new Form("index.php?module=config-fcverw", "post");
+          $form_container = new FormContainer('Alle L&auml;nder');
+          $form_container->output_row_header('ID');
+          $form_container->output_row_header('Landname');
+          $form_container->output_row_header('Landart');
+          $form_container->output_row_header('L&auml;nderinfos');
+  	      $form_container->output_row_header('Diplomatie');
+          $form_container->output_row_header('Verwandtschaften');
+  	      $form_container->output_row_header('Bewohner');
+  	      $form_container->output_row_header('Bearbeiten?');
+
+          // Hier werden die Lander ausgelesen
+
+
+          $form_container->end();
+          $form->end();
+        } // Ende der Startseite
+
+
+
+	// start the fire with showing ALL continents, while there is neither set action
+	// nor it is show_all. Yay!
+			  if ($mybb->input['action'] == "kontinente")
+			  {
+					$page->add_breadcrumb_item('&Uuml;bersicht aller Kontinente');
+
+			    $page->output_header('L&auml;nderverwaltung - &Uuml;bersicht aller Kontinente');
+
+			    // which tab is selected?
+			    $page->output_nav_tabs($sub_tabs, 'kontinente');
+
+			    // now start the form
+			    $form = new Form("index.php?module=config-fcverw", "post");
+			    $form_container = new FormContainer('Alle Kontinente');
+			    $form_container->output_row_header('ID', array("class" => "align_center", "width" => "3%"));
+			    $form_container->output_row_header('Kontinentname', array("class" => "align_center", "width" => "15%"));
+					$form_container->output_row_header('Beschreibung', array("class" => "align_center"));
+					$form_container->output_row_header('Regionen', array("class" => "align_center", "width" => "5%"));
+					$form_container->output_row_header('L&auml;nder', array("class" => "align_center", "width" => "5%"));
+					$form_container->output_row_header('Optionen', array("class" => "align_center", "width" => "15%"));
+
+			    // Hier werden die Kontinente ausgelesen
+					$fc_kontsel = $db->write_query("SELECT * FROM ".TABLE_PREFIX."laender_kontinente ORDER BY kname");
+
+					while ($row = $db->fetch_array($fc_kontsel))
+					{
+						// Auslesen der Anzahl der Regionen und Länder
+						$regionen = $db->num_rows($db->simple_select("laender_regionen", "rid", "rkid = {$row['kid']}"));
+						$laender = $db->num_rows($db->simple_select("laender", "landid", "lkid = {$row['kid']}"));
+
+						$form_container->output_cell($row['kid'], array("class" => "align_center"));
+						$form_container->output_cell("<b>".$row['kname']."</b>");
+						$form_container->output_cell($row['kbeschr']);
+						$form_container->output_cell($regionen, array("class" => "align_center"));
+						$form_container->output_cell($laender, array("class" => "align_center"));
+
+						// Optionen-Fach basteln
+						//erst pop up dafür bauen - danke an @Risuena
+					  $popup = new PopupMenu("fcverw_{$row['kid']}", "Optionen");
+					  $popup->add_item(
+					  	"Editieren",
+					    "index.php?module=config-fcverw&amp;action=edit_kontinent&amp;kid={$row['kid']}"
+					  );
+						$popup->add_item(
+					  	"L&ouml;schen",
+					    "index.php?module=config-fcverw&amp;action=del_kontinent&amp;kid={$row['kid']}"
+					  );
+						$form_container->output_cell($popup->fetch(), array("class" => "align_center"));
+
+						$form_container->construct_row(); // Reihe erstellen
+					}
+
+			    $form_container->end();
+			    $form->end();
+			  } // Ende der Kontinentübersicht
+
+
+	// start with entering new continents -
+	// or do the database entry
+				if ($mybb->input['action'] == "add_kontinent")
+				{
+					if ($mybb->request_method == 'post' && $mybb->input['kname'] != '')
+					{
+						$insert_query = array(
+							'kname' => htmlspecialchars_uni($mybb->input['kname']),
+							'kbeschr' => htmlspecialchars_uni($mybb->input['kbeschr'])
+						);
+
+						if ($db->insert_query("laender_kontinente", $insert_query))
+						{
+							redirect("admin/index.php?module=config-fcverw&action=kontinente");
+						}
+
+					}
+					else
+					{
+						if ((!$mybb->input['kname'] || $mybb->input['kname'] == '') && $mybb->request_method == 'post')
+						{
+							$l_fehler = " <b><font color='#ff0000'>Der Kontinentname muss ausgef&uuml;llt sein!</font></b>";
+						}
+
+						$page->add_breadcrumb_item('Kontinent anlegen');
+
+						$page->output_header('L&auml;nderverwaltung - Kontinent anlegen');
+
+						// which tab is selected? here: add_kontinent
+						$page->output_nav_tabs($sub_tabs, 'add_kontinent');
+
+						$form = new Form("index.php?module=config-fcverw&amp;action=add_kontinent", "post", "", 1);
+						$form_container = new FormContainer('Neuen Kontinent anlegen');
+
+						// der name
+						$form_container->output_row(
+							'Name des Kontinents'.$l_fehler,
+							'Vollst&auml;ndiger Name des Kontinents',
+							$form->generate_text_box(
+								'kname',
+								htmlspecialchars_uni($mybb->input['kname']),
+								array('style' => 'width: 200px;')
+							)
+						);
+
+						// Informationstext
+						$form_container->output_row(
+							'Beschreibung',
+							'Gibt es interessante Informationen &uuml;ber den Kontinent?',
+							$form->generate_text_area(
+								'kbeschr',
+								$db->escape_string($mybb->input['kbeschr'])
+							)
+						);
+
+
+						$form_container->end();
+						$button[] = $form->generate_submit_button('Kontinent anlegen');
+						$form->output_submit_wrapper($button);
+						$form->end();
+					}
+				}
+
+
+	// start with editing continents -
+	// or do the database entry
+				if ($mybb->input['action'] == "edit_kontinent")
+				{
+					// Eintrag machen
+					if ($mybb->request_method == 'post' && $mybb->input['kname'] != '')
+					{
+						$update_query = array(
+							'kname' => htmlspecialchars_uni($mybb->input['kname']),
+							'kbeschr' => htmlspecialchars_uni($mybb->input['kbeschr'])
+						);
+
+						if ($db->update_query("laender_kontinente", $update_query, "kid = ".(int)$mybb->input['kid']))
+						{
+							redirect("admin/index.php?module=config-fcverw&action=kontinente");
+						}
+
+					}
+					else
+					{
+						// Formular anzeigen
+						$sub_tabs['edit_kontinent'] = array(
+							'title' => 'Kontinent editieren',
+							'link' => 'index.php?module=config-fcverw&amp;action=edit_kontinent&amp;kid='.$mybb->input['kid'],
+							'description' => 'Editieren eines bestehenden Kontinents'
+						);
+
+						$page->add_breadcrumb_item('Kontinent editieren');
+
+						$page->output_header('L&auml;nderverwaltung - Kontinent editieren');
+
+						// which tab is selected? here: edit_kontinent - der ist NICHT permanent!
+						$page->output_nav_tabs($sub_tabs, 'edit_kontinent');
+
+						$form = new Form("index.php?module=config-fcverw&amp;action=edit_kontinent", "post", "", 1);
+						$form_container = new FormContainer('Kontinent editieren');
+						// ID mitgeben
+						echo $form->generate_hidden_field('kid', $mybb->input['kid']);
+
+						// Daten holen
+						$dataget = $db->simple_select("laender_kontinente", "*", "kid = {$mybb->input['kid']}");
+						$data = $db->fetch_array($dataget);
+
+						// Fehlermeldung ausgeben, wenn Name nicht ausgefüllt
+						if ((!$mybb->input['kname'] || $mybb->input['kname'] == '') && $mybb->request_method == 'post')
+						{
+							$l_fehler = " <b><font color='#ff0000'>Der Kontinentname muss ausgef&uuml;llt sein!</font></b>";
+							$data['kname'] = $mybb->input['kname'];
+							$data['kbeschr'] = $mybb->input['kbeschr'];
+						}
+
+						// der name
+						$form_container->output_row(
+							'Name des Kontinents'.$l_fehler,
+							'Vollst&auml;ndiger Name des Kontinents',
+							$form->generate_text_box(
+								'kname',
+								htmlspecialchars_uni($data['kname']),
+								array('style' => 'width: 200px;')
+							)
+						);
+
+						// Informationstext
+						$form_container->output_row(
+							'Beschreibung',
+							'Gibt es interessante Informationen &uuml;ber den Kontinent?',
+							$form->generate_text_area(
+								'kbeschr',
+								$db->escape_string($data['kbeschr'])
+							)
+						);
+
+						$form_container->end();
+						$button[] = $form->generate_submit_button('Kontinent editieren');
+						$form->output_submit_wrapper($button);
+						$form->end();
+					}
+				} // Ende Editieren Kontinent
+
+
+	// start with deleting continents
+	// or do the database entry
+				if ($mybb->input['action'] == "del_kontinent")
+				{
+					$kid = (int)$mybb->input['kid'];
+
+					// Länder updaten
+					$update_laender = array(
+						'lkid' => "0"
+					);
+					$db->update_query("laender", $update_laender, "lkid = ".$kid);
+
+					// Regionen updaten
+					$update_regionen = array(
+						'rkid' => "0"
+					);
+					$db->update_query("laender_regionen", $update_regionen, "rkid = ".$kid);
+
+					// Eintrag löschen
+					if ($db->delete_query("laender_kontinente", "kid = ".$kid))
+					{
+						redirect("admin/index.php?module=config-fcverw&action=kontinente");
+					}
+
+				} // Ende Löschen Kontinent
+
+
+
+        $page->output_footer();
+  			exit;
+  		}
+
+  	} // Ende der Funktion
